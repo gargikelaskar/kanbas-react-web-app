77c9e1ab566901c51386d43b9af354068ec69c5d
unknown <gargikelaskar@gmail.com>
2024-02-16 21:02:48 -0500

    Creating Single Page Applications with React.js

---
+++ src/Kanbas/index.tsx
@@ -0,0 +1,25 @@
+import Dashboard from "./Dashboard";
+import KanbasNavigation from "./Navigation";
+import { Routes, Route } from "react-router-dom";
+import { Navigate } from "react-router";
+import Courses from "./Courses";
+
+function Kanbas() {
+  return (
+    <div className="d-flex">
+      <div style={{ height: 100 }}>
+        <KanbasNavigation />
+      </div>
+      <div style={{ flexGrow: 1 }}>
+        <Routes>
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Account" element={<h1>Account</h1>} />
+          <Route path="/Courses/:courseid/*" element={<Courses />} />
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default Kanbas;
